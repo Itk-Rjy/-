7caebf145174dfe89da1d4995289564834c62a10
Itk-Rjy <kshelyakov8@gmail.com>
2023-08-15 16:05:44 +0300

    сравнение ссылок
    сравнение ссылок

--- main.py
+++ main.py
@@ -1,4 +1,13 @@
-f1 = file('File_input1.txt'.open)
-f2 = file('File_input2.txt'.open)
+f1 = open('File_input1.txt', 'r')
+f2 = open('File_input2.txt', 'r')
+f3_sort = open('File_input3.txt', 'r')
 
+for line1 in f1:
+    for line2 in f2:
+        if line1 == line2:
+            f3_sort.write("%s\n" %(line1))
+
+f3_sort.close()
+f1.close()
+f2.close()
 
