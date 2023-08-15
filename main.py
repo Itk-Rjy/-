3986d4ac772c46f60de02870dbfcc20ace0f2a05
Itk-Rjy <kshelyakov8@gmail.com>
2023-08-15 16:49:53 +0300

    сравнение ссылок
    сохранение несовпадающих ссылок в 4 файл

--- main.py
+++ main.py
@@ -1,13 +1,18 @@
 f1 = open('File_input1.txt', 'r')
 f2 = open('File_input2.txt', 'r')
-f3_sort = open('File_input3.txt', 'r')
+f3_sort = open('File_input3.txt', 'w')
+f4_sort = open('File_input4.txt', 'w')
 
 for line1 in f1:
     for line2 in f2:
-        if line1 == line2:
-            f3_sort.write("%s\n" %(line1))
+        line3 = line1.replace('!','')
+        line4 = line2.replace('!','')
+        if line3 == line4:
+            f3_sort.write("%s"%(line2))
+        else:
+            f4_sort.write("%s"%(line2))
 
-f3_sort.close()
 f1.close()
 f2.close()
-
+f3_sort.close()
+f4_sort.close()
